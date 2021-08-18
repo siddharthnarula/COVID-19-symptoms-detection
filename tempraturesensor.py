3d7b6ad3cf145a5f3830ea1f2fbfc83e92bf1b58
Siddharth Narula <31994579+siddharthnarula1234@users.noreply.github.com>
2021-08-17 18:33:21 -0700

    Create tempraturesensor.py

---
+++ tempraturesensor.py
@@ -0,0 +1,9 @@
+import js2py
+setChartLibrary('google-chart');
+setChartTitle('Temperature_Sensor');
+setChartType('lineGraph');
+setAxisName('time_stamp','Temperature');
+add(183);
+mul(0.0977);
+mul(1.8);
+plotChart('time_stamp','pina0')
